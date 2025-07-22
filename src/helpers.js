d30ad421c91db9562b98a86a1ffdb2e29c5a76dd
kamil <kamil.mrowka@outlook.com.au>
2025-07-22 17:09:37 +0200

    Add helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,2 +1,26 @@
-const appendElement = () => {};
-const createDiv = () => {};
+const createDiv = ({className = '', styles = {}, id}) => {
+  const div = createElement({tag: 'div', className, styles, id});
+  return div;
+};
+
+const appendElement = (parent, child) => {
+  parent.appendChild(child);
+  return child;
+};
+
+const removeElement = (element) => {
+  if (element && element.parentNode) {
+    element.parentNode.removeChild(element);
+  }
+  return element;
+};
+
+const createElement = ({tag, className = '', styles = {}, id}) => {
+  const element = document.createElement(tag);
+  if (className) element.className = className;
+  if (id) element.id = id;
+  Object.assign(element.style, styles);
+  return element;
+};
+
+export {createElement, createDiv, removeElement, appendElement};
