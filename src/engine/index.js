96ca30e53f1ccbc4c73dff1f0e8311f751dee73c
kamil <kamil.mrowka@outlook.com.au>
2025-07-25 16:55:02 +0200

    Add engine directory and moved previously deleted files

---
+++ src/engine/index.js
@@ -0,0 +1,4 @@
+// Engine entry point for weekly-calendar-engine package
+export {default as ColumnObserver, COLORS} from './event-manager.js';
+export {createEngine} from './engine.js';
+export * from './helpers.js';
